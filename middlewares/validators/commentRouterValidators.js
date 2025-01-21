e9844c84d9815330e691b0852fb5d5f280fa3e49
Shevtsiv Nikita <nikita0503ua@gmail.com>
2025-01-21 15:16:06 +0200

    fixes

--- middlewares/validators/commentRouterValidators.js
+++ middlewares/validators/commentRouterValidators.js
@@ -1,4 +1,5 @@
 const {check} = require('express-validator');
+const path = require('path');
 
 const getFullCommentValidators = () => {
     return [
@@ -20,6 +21,28 @@ const createCommentValidators = () => {
         check('projectId').isNumeric().withMessage('Must be a number'),
         check('taskId').isNumeric().withMessage('Must be a number'),
         check('message').notEmpty().withMessage('Message is required'),
+        check('file').custom((value, { req }) => {
+            let files = req?.files?.file;
+            if(!files){
+                return true;
+            }
+            if(!files?.length && files?.name){
+                files = [files]
+            }
+            console.log({files})
+            for(let i = 0; i < files.length; i++){
+                var extension = (path.extname(files[i].name)).toLowerCase();
+                console.log({extension})
+                if(extension !== '.jpg'
+                    && extension !== '.jpeg'
+                    && extension !== '.png'
+                    && extension !== '.gif'
+                    && extension !== '.webp'){
+                    return false;
+                }
+            }
+            return true;
+        }).withMessage('Files should be images'),
     ];  
 };
 
@@ -29,6 +52,26 @@ const editCommentValidators = () => {
         check('taskId').isNumeric().withMessage('Must be a number'),
         check('commentId').isNumeric().withMessage('Must be a number'),
         check('message').notEmpty().withMessage('Must be a number'),
+        check('file').custom((value, { req }) => {
+            let files = req?.files?.file;
+            if(!files){
+                return true;
+            }
+            if(!files?.length && files?.name){
+                files = [files]
+            }
+            for(let i = 0; i < files.length; i++){
+                var extension = (path.extname(files[i].name)).toLowerCase();
+                if(extension !== '.jpg'
+                    && extension !== '.jpeg'
+                    && extension !== '.png'
+                    && extension !== '.gif'
+                    && extension !== '.webp'){
+                    return false;
+                }
+            }
+            return true;
+        }).withMessage('Files should be images'),
     ];  
 };
 
